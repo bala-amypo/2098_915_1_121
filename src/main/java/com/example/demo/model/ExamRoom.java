1ff1a7c3f517b85b21751df065e0d6f6e7974c36
bala-amypo <bala@amypo.in>
2025-12-18 02:58:31 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamRoom.java
+++ src/main/java/com/example/demo/model/ExamRoom.java
@@ -1,5 +1,51 @@
 package com.example.demo.model;
 
 public class ExamRoom {
-    
+     private Long id;
+
+    private String roomNumber;
+    private Integer capacity;
+    private Integer rows;
+    private Integer columns;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public Integer getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+    public Integer getRows() {
+        return rows;
+    }
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+    public Integer getColumns() {
+        return columns;
+    }
+    public void setColumns(Integer columns) {
+        this.columns = columns;
+    }
+    public ExamRoom(Long id, String roomNumber, Integer capacity, Integer rows, Integer columns) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.rows = rows;
+        this.columns = columns;
+    }
+    public ExamRoom() {
+    }
 }
+
+
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,49 @@
 package com.example.demo.model;
 
-public class Student {
-    
+public class Student{
+    private Long id;
+    private String rollNumber;
+    private String name;
+    private String department;
+    private Integer year;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRollNumber() {
+        return rollNumber;
+    }
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+    public Student(Long id, String rollNumber, String name, String department, Integer year) {
+        this.id = id;
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+    }
+    public Student() {
+    }
 }
+
