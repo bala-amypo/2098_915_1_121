13fe8243ce970968e7e51a7a5a2fd04f6e620ab2
bala-amypo <bala@amypo.in>
2025-12-18 02:59:59 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamSession.java
+++ src/main/java/com/example/demo/model/ExamSession.java
@@ -1,5 +1,45 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
 public class ExamSession {
+    private Long id;
+
+    private String courseCode;
+    private LocalDate examDate;
+    private String examTime;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCourseCode() {
+        return courseCode;
+    }
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+    public void setExamDate(LocalDate examDate) {
+        this.examDate = examDate;
+    }
+    public String getExamTime() {
+        return examTime;
+    }
+    public void setExamTime(String examTime) {
+        this.examTime = examTime;
+    }
+    public ExamSession(Long id, String courseCode, LocalDate examDate, String examTime) {
+        this.id = id;
+        this.courseCode = courseCode;
+        this.examDate = examDate;
+        this.examTime = examTime;
+    }
+    public ExamSession() {
+    }
+    private students;
     
 }
