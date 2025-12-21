40437224ff5172a005fdb50f5cec61b23db0061b
bala-amypo <bala@amypo.in>
2025-12-21 14:10:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
 @RestController
-@RequestMapping("/auth")
-
 public class AuthController {
 
     private final UserService userService;
@@ -16,13 +13,15 @@ public class AuthController {
         this.userService = userService;
     }
 
+    // THIS WILL NOW MAP TO "/"
+    @GetMapping("/")
+    public String root() {
+        return "OK";
+    }
+
+    // This will map to "/register"
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userService.registerUser(user);
     }
-    @GetMapping("/")
-public String root() {
-    return "Application is running";
-}
-
 }
