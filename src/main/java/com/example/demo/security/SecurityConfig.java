cdab3fd69c634090d9fdcc2a751d67961ed0655c
bala-amypo <bala@amypo.in>
2025-12-21 21:12:20 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,21 +1,15 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeHttpRequests()
-            .anyRequest().permitAll(); // allow all requests
-        return http.build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
