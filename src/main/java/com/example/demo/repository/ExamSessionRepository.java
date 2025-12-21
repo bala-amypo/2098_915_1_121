b7287e30ff7dbe1fc7b7ab18e612aa5bf432dece
bala-amypo <bala@amypo.in>
2025-12-21 13:54:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamSessionRepository.java
+++ src/main/java/com/example/demo/repository/ExamSessionRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.ExamSession;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.ExamSession;
 
 public interface ExamSessionRepository extends JpaRepository<ExamSession, Long> {
 
-    // Optional<ExamSession> findByCourseCode(String courseCode);
-
+    Optional<ExamSession> findByCourseCode(String courseCode);
 }
+
+
