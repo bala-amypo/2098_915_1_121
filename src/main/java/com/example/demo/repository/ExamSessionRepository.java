539356ce7692f85fb2c7c67c9dbd4ccc21116be8
bala-amypo <bala@amypo.in>
2025-12-21 19:25:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamSessionRepository.java
+++ src/main/java/com/example/demo/repository/ExamSessionRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.repository;
 
+
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.demo.model.ExamSession;
+
 
 @Repository
 public interface ExamSessionRepository extends JpaRepository<ExamSession, Long> {
-    List<ExamSession> findByExamDate(LocalDate date);
+List<ExamSession> findByExamDate(LocalDate date);
 }
--- src/main/java/com/example/demo/repository/SeatingPlanRepository.java
+++ src/main/java/com/example/demo/repository/SeatingPlanRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
 
-import java.util.List;
+
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.demo.model.SeatingPlan;
+
 
 @Repository
 public interface SeatingPlanRepository extends JpaRepository<SeatingPlan, Long> {
-    List<SeatingPlan> findByExamSessionId(Long sessionId);
+List<SeatingPlan> findByExamSessionld(Long sessionld);
 }
