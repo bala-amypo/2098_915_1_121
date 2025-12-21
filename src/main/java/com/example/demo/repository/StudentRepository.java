b31940c477fc28f2a1ebd8ada2dddc99d95b33e4
bala-amypo <bala@amypo.in>
2025-12-21 15:05:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByRollNumber(String rollNumber);
 }
