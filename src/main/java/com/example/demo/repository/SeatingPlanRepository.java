4736f67f1067e708474382c7567c944185970488
bala-amypo <bala@amypo.in>
2025-12-21 20:31:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatingPlanRepository.java
+++ src/main/java/com/example/demo/repository/SeatingPlanRepository.java
@@ -1,15 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.SeatingPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.SeatingPlan;
-
-@Repository
 public interface SeatingPlanRepository extends JpaRepository<SeatingPlan, Long> {
 
-    // IMPORTANT: Id (capital I)
+    // Corrected method name
     List<SeatingPlan> findByExamSessionId(Long examSessionId);
 }
--- src/main/java/com/example/demo/service/SeatingPlanService.java
+++ src/main/java/com/example/demo/service/SeatingPlanService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import com.example.demo.model.SeatingPlan;
+
+import java.util.List;
 
 public interface SeatingPlanService {
 
-    List<SeatingPlan> getSeatingPlanByExamSession(Long examSessionId);
+    List<SeatingPlan> getSeatingPlansByExamSessionId(Long examSessionId);
 }
