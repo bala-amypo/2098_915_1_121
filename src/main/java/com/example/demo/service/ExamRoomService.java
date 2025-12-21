d9269af5c8c9cfc7ae47fa54c7655eb4cf3cc415
bala-amypo <bala@amypo.in>
2025-12-21 19:28:34 +0000

    User commit

--- src/main/java/com/example/demo/service/ExamRoomService.java
+++ src/main/java/com/example/demo/service/ExamRoomService.java
@@ -1,11 +1,11 @@
 package com.example.demo.service;
 
-import java.util.List;
+
+import java.util.*;
 import com.example.demo.model.ExamRoom;
 
+
 public interface ExamRoomService {
-
-    ExamRoom addRoom(ExamRoom room);
-
-    List<ExamRoom> getAllRooms();
+ExamRoom addRoom(ExamRoom room);
+List<ExamRoom> getAllRooms();
 }
