f77e2f6599ba7f303402c267dc9bdc64ee904a32
bala-amypo <bala@amypo.in>
2025-12-21 05:56:56 +0000

    User commit

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,49 +1,52 @@
 package com.example.demo.model;
 
-public class Student{
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
     private Long id;
-    private String rollNumber;
     private String name;
     private String department;
-    private Integer year;
+    private int year;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String department, int year) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+    }
+
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public String getRollNumber() {
-        return rollNumber;
-    }
-    public void setRollNumber(String rollNumber) {
-        this.rollNumber = rollNumber;
-    }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDepartment() {
         return department;
     }
     public void setDepartment(String department) {
         this.department = department;
     }
-    public Integer getYear() {
+
+    public int getYear() {
         return year;
     }
-    public void setYear(Integer year) {
+    public void setYear(int year) {
         this.year = year;
-    }
-    public Student(Long id, String rollNumber, String name, String department, Integer year) {
-        this.id = id;
-        this.rollNumber = rollNumber;
-        this.name = name;
-        this.department = department;
-        this.year = year;
-    }
-    public Student() {
     }
 }
-
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,52 +1,52 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    private Long id;
-    private String name;
-    private String email;
+
+    @Id
+    private int userId;
+    private String username;
     private String password;
-    private String role="STAFF";
-    public Long getId() {
-        return id;
+    private String role;
+
+    public User() {
     }
-    public void setId(Long id) {
-        this.id = id;
+
+    public User(int userId, String username, String password, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
-    public String getName() {
-        return name;
+
+    public int getUserId() {
+        return userId;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
-    public String getEmail() {
-        return email;
+
+    public String getUsername() {
+        return username;
     }
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getRole() {
         return role;
     }
     public void setRole(String role) {
         this.role = role;
     }
-    public User(Long id, String name, String email, String password, String role) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-    public User() {
-    }
 }
-
-
-
-
