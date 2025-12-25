89dea7e7dd09aae938bf1bb3793b24d67f8cefbb
bala-amypo <bala@amypo.in>
2025-12-25 15:42:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -1,32 +1,5 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.ExamSession;
-import com.example.demo.model.Student;
-import com.example.demo.repository.ExamSessionRepository;
-import com.example.demo.service.ExamSessionService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExamSessionServiceImpl implements ExamSessionService {
-
-    private final ExamSessionRepository repo;
-
-    public ExamSessionServiceImpl(ExamSessionRepository repo) {
-        this.repo = repo;
-    }
-
-    @Override
-    public ExamSession saveSession(ExamSession session) {
-        if (session.getStudents() != null) {
-            for (Student s : session.getStudents()) {
-                s.setExamSession(session);
-            }
-        }
-        return repo.save(session);
-    }
-}
-package com.example.demo.service.impl;
-
 import com.example.demo.exception.ApiException;
 import com.example.demo.model.ExamSession;
 import com.example.demo.repository.ExamSessionRepository;
