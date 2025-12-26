b19d88dc16eeb7c379fc681f7411a56a8c0e6f4c
bala-amypo <bala@amypo.in>
2025-12-26 16:49:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,16 +1,1 @@
-package com.example.demo.controller;
-
-import com.example.demo.model.Student;
-import com.example.demo.service.StudentService;
-import java.util.List;
-
-public class StudentController {
-
-    private StudentService service;
-
-    public StudentController(StudentService s){ this.service = s; }
-    public StudentController(){}
-
-    public Student add(Student s){ return s; }
-    public List<Student> list(){ return List.of(); }
-}
+`
