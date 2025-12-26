b54fd48047ed0cf2b8f42d029ac312dadf60109e
bala-amypo <bala@amypo.in>
2025-12-26 10:11:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.exception.ApiException;
-import com.example.demo.entity.User;   // adjust if model package differs
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.JWTTokenProvider;
 import com.example.demo.service.AuthService;
